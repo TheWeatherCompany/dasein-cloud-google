4294119fe9ab6efbc3d52e7a13bb5195dc42b757
igoonich <igoonich@evelopers.com>
2014-10-16 17:28:50 +0400

    fix issue QUOTA_EXCEEDED error which result into 403 error code (#24)

--- src/main/java/org/dasein/cloud/google/common/GoogleOperationFailedException.java
+++ src/main/java/org/dasein/cloud/google/common/GoogleOperationFailedException.java
@@ -1,6 +1,7 @@
 package org.dasein.cloud.google.common;
 
 import com.google.api.services.compute.model.Operation;
+import com.google.common.collect.Maps;
 import org.dasein.cloud.CloudErrorType;
 import org.dasein.cloud.CloudException;
 
@@ -8,6 +9,9 @@ import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Map;
+
+import static com.google.api.services.compute.model.Operation.Error.Errors;
 
 /**
  * Exceptions to be using when an operation fails on GCE side
@@ -16,22 +20,36 @@ import java.util.List;
  * @since 01.05.2014
  */
 public class GoogleOperationFailedException extends CloudException {
+
 	private static final long serialVersionUID = -4100288452798844941L;
 
-	public GoogleOperationFailedException(@Nonnegative int httpCode, @Nullable String providerCode, @Nonnull String msg) {
+	private static final Map<String, Integer> OPERATION_CODES = Maps.newHashMap();
+
+	static {
+		OPERATION_CODES.put("QUOTA_EXCEEDED", 400);
+	}
+
+	private GoogleOperationFailedException(@Nonnegative int httpCode, @Nullable String providerCode, @Nonnull String msg) {
 		super(CloudErrorType.GENERAL, httpCode, providerCode, msg);
 	}
 
-	public GoogleOperationFailedException(@Nonnegative int httpCode, @Nullable String providerCode, @Nonnull String msg, @Nonnull Throwable cause) {
+	private GoogleOperationFailedException(@Nonnegative int httpCode, @Nullable String providerCode, @Nonnull String msg, @Nonnull Throwable cause) {
 		super(CloudErrorType.GENERAL, httpCode, providerCode, msg, cause);
 	}
 
 	public static GoogleOperationFailedException create(Operation operation) {
-		List<Operation.Error.Errors> errors = operation.getError().getErrors();
+		List<Errors> errors = operation.getError().getErrors();
 		return create(operation, errors.get(0).getMessage());
 	}
 
 	public static GoogleOperationFailedException create(Operation operation, String errorMessage) {
-		return new GoogleOperationFailedException(operation.getHttpErrorStatusCode(), operation.getHttpErrorMessage(), errorMessage);
+		List<Errors> errors = operation.getError().getErrors();
+
+		Errors firstError = errors.get(0);
+		int httpStatusCode = OPERATION_CODES.containsKey(firstError.getCode())
+				? OPERATION_CODES.get(firstError.getCode()) : operation.getHttpErrorStatusCode();
+
+		return new GoogleOperationFailedException(httpStatusCode, operation.getHttpErrorMessage(), errorMessage);
 	}
+
 }
